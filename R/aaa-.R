86c3615d1891eb5d0dc9ac9cea1f1c84579bab8b
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2022-08-06 16:07:34 +0200

    Additional imports.

--- R/aaa-.R
+++ R/aaa-.R
@@ -1,1 +1,22 @@
-#' @importFrom kali map_details image.plot
+#' @importFrom kali map_details image.map
+NULL
+#' @importFrom ncdf4 nc_open nc_close ncvar_get ncatt_get
+NULL
+#' @importFrom nctools ncvar_dim write_ncdf
+NULL
+#' @importFrom fields poly.image image.plot interp.surface.grid tim.colors
+#' imageplot.info imageplot.setup
+NULL
+#' @importFrom maptools map2SpatialPolygons
+NULL
+#' @importFrom lubridate yday<- year month ymd leap_year parse_date_time period
+#' ymd_hms days_in_month parse_date_time
+NULL
+#' @importFrom mgcv gam
+NULL
+#' @importFrom akima interp interpp bilinear bicubic
+NULL
+#' @importFrom sp SpatialPoints over CRS proj4string
+NULL
+#' @importFrom colorful divergencePalette
+NULL

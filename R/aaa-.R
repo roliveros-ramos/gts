6da8204d4368baefad279934eb00696382fe63ba
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2023-07-22 02:02:42 +0200

    export nctools::write_ncdf so it's available without loading nctools.

--- R/aaa-.R
+++ R/aaa-.R
@@ -4,13 +4,15 @@ NULL
 NULL
 #' @importFrom nctools ncvar_dim write_ncdf
 NULL
+#' @export write_ncdf
+NULL
 #' @importFrom fields poly.image image.plot interp.surface.grid tim.colors
 #' imageplot.info imageplot.setup
 NULL
 #' @importFrom maptools map2SpatialPolygons
 NULL
 #' @importFrom lubridate yday<- year month ymd leap_year parse_date_time period
-#' ymd_hms days_in_month parse_date_time
+#' ymd_hms days_in_month parse_date_time days_in_month seconds
 NULL
 #' @importFrom mgcv gam
 NULL
@@ -19,4 +21,6 @@ NULL
 #' @importFrom sp SpatialPoints over CRS proj4string
 NULL
 #' @importFrom colorful divergencePalette
+NULL
+#' @importFrom stats gaussian
 NULL

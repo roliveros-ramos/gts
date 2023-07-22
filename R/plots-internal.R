b5a459676efc35e474b10706022a0870fa6e4f87
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2023-07-22 02:07:54 +0200

    renaming arguments of plot.gts: time and depth now.

--- R/plots-internal.R
+++ R/plots-internal.R
@@ -1,9 +1,15 @@
 
 #' @export
-plot.gts = function(x, slice=1, ...) {
+plot.gts = function(x, time=1, depth=1, ...) {
   if(mode(x$x)=="logical") x$x = 0L + x$x
-  image.map(x$grid$LON, x$grid$LAT, x$x, slice=slice, ...)
-  lab = sprintf("%s (%s)", x$info$varid, x$time[slice])
+  if(length(dim(x$x))==4) {
+    image.map(x$grid$LON, x$grid$LAT, x$x[,,depth,], slice=time, ...)
+    lab = sprintf("%s (depth=%s, time=%s)", x$info$varid, x$depth[depth], x$time[time])
+    title(main=lab)
+    return(invisible(NULL))
+  }
+  image.map(x$grid$LON, x$grid$LAT, x$x, slice=time, ...)
+  lab = sprintf("%s (time=%s)", x$info$varid, x$time[time])
   title(main=lab)
   return(invisible(NULL))
 }

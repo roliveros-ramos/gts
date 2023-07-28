ada73cffb6c38545124f40246287d9fafd9363c1
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2023-07-28 22:11:36 +0200

    fix main title in plot.gts. Bumping up version.

--- R/plots-internal.R
+++ R/plots-internal.R
@@ -1,15 +1,16 @@
 
 #' @export
 plot.gts = function(x, time=1, depth=1, ...) {
+  args = list(...)
   if(mode(x$x)=="logical") x$x = 0L + x$x
   if(length(dim(x$x))==4) {
     image.map(x$grid$LON, x$grid$LAT, x$x[,,depth,], slice=time, ...)
     lab = sprintf("%s (depth=%s, time=%s)", x$info$varid, x$depth[depth], x$time[time])
-    title(main=lab)
-    return(invisible(NULL))
+  } else {
+    image.map(x$grid$LON, x$grid$LAT, x$x, slice=time, ...)
+    lab = sprintf("%s (time=%s)", x$info$varid, x$time[time])
   }
-  image.map(x$grid$LON, x$grid$LAT, x$x, slice=time, ...)
-  lab = sprintf("%s (time=%s)", x$info$varid, x$time[time])
-  title(main=lab)
+  main = if(is.null(args$main)) lab else NULL
+  title(main=main)
   return(invisible(NULL))
 }
